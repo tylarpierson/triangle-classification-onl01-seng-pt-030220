48dc4918a7afe5421cc0c354c9d6d2caa5c448b5
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-04-06 19:01:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,8 @@
 class Triangle
-  
+  attr_accessor :equilateral, :isosceles, :scalene 
+  def initialize(equilateral, isosceles, scalene)
+    @equilateral = equilateral
+    @isosceles = isosceles
+    @scalene = scalene
+  end 
 end
