c164c91c9008cd99b9af42a00e4fb3dc4b88b1a3
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-04-06 19:08:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
     elsif (@side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 || @side2 + @side3 <= @side1)
       raise TriangleError
     else 
-      
+      if (@side1 == @side2 && @side1 == @side3)
   end 
   
   class TriangleError < StandardError 
