3650bbf8e35cb632e1f7933cfd058e8e6ae5e27f
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-04-06 19:09:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,10 @@ class Triangle
         :equilateral
       elsif (@side1 == @side2 || @side2 == @side 3 || @side1 == @side3)
         :isosceles 
-      elsif () 
+      elsif (@side1 != @side2) && (@side2 != @side3) && (@side1 != @side3)
+        :scalene
+      end 
+    end 
   end 
   
   class TriangleError < StandardError 
